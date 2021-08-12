3a1efa8f4c1d5ab1d66c3b8cc237e5e4bd015aed
Moses Kim <mosesrandykim@gmail.com>
2021-08-12 15:43:43 -0700

    added instructions for checkPalindrom

--- check-palindrome.js
+++ check-palindrome.js
@@ -1,3 +1,28 @@
+/*
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] string inputString
+
+A non-empty string consisting of lowercase characters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 105.
+
+[output] boolean
+
+true if inputString is a palindrome, false otherwise.
+*/
+
 function checkPalindrome(inputString) {
   let word = '';
   let reverseWord = '';
