ca28d2e7b7facabbc758ea6d4f9c4b8ab426f520
Moses Kim <mosesrandykim@gmail.com>
2021-08-12 15:45:25 -0700

    added instructions for shapeArea

--- shape-area.js
+++ shape-area.js
@@ -1,3 +1,24 @@
+/*
+Example
+
+For n = 2, the output should be
+shapeArea(n) = 5;
+For n = 3, the output should be
+shapeArea(n) = 13.
+Input / Output
+
+[execution time limit]4 seconds(js)
+
+[input] integer n
+
+Guaranteed constraints:
+1 ≤ n < 104.
+
+[output] integer
+
+The area of the n - interesting polygon.
+*/
+
 function shapeArea(n) {
   let area = 1;
   for (let i = 1; i < n; i++) {
