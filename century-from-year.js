e540ed22216fdbd8d4c358d96787109648866861
Moses Kim <mosesrandykim@gmail.com>
2021-08-12 15:42:46 -0700

    added instructions for centuryFromYear

--- century-from-year.js
+++ century-from-year.js
@@ -1,3 +1,26 @@
+/*
+Example
+
+For year = 1905, the output should be
+centuryFromYear(year) = 20;
+For year = 1700, the output should be
+centuryFromYear(year) = 17.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] integer year
+
+A positive integer, designating the year.
+
+Guaranteed constraints:
+1 ≤ year ≤ 2005.
+
+[output] integer
+
+The number of the century the year is in.
+*/
+
 function centuryFromYear(year) {
   const century = Math.ceil(year / 100);
   return century;
